e965b8a44c8677f766af738099b88ab25b5b08e6
syedf1102 <syedf1102@gmail.com>
2023-06-18 21:01:38 -0700

    rent class

---
+++ src/main/java/com/tw/controller/RentController.java
@@ -0,0 +1,48 @@
+package com.tw.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tw.model.Rent;
+import com.tw.service.RentService;
+
+import jakarta.websocket.server.PathParam;
+
+@RestController
+@RequestMapping("/rent")
+public class RentController {
+
+	@Autowired
+	private RentService rentService;
+
+	@PostMapping("/save")
+	public String saveUser(@RequestBody Rent rent) {
+		String ret = rentService.saveRent(rent);
+		return ret;
+	}
+
+	@GetMapping("/listrent")
+	public List<Rent> getRent() {
+		return rentService.getRent();
+
+	}
+
+	@GetMapping("/rentById")
+	public Rent getRentById(@PathParam("id") Long id) {
+		return rentService.getRentById(id);
+
+	}
+
+	@GetMapping("/deleteById")
+	public Rent deleteById(@PathParam("id") Long id) {
+		return rentService.delete(id);
+
+	}
+
+}
---
+++ src/main/java/com/tw/serviceImp/RentServiceImp.java
@@ -0,0 +1,44 @@
+package com.tw.serviceImp;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tw.model.Rent;
+import com.tw.repository.RentRepository;
+import com.tw.service.RentService;
+
+@Service
+public class RentServiceImp implements RentService {
+
+	@Autowired
+	RentRepository rentRepository;
+
+	@Override
+	public String saveRent(Rent rent) {
+		rentRepository.save(rent);
+		return "Save Success!";
+	}
+
+	@Override
+	public List<Rent> getRent() {
+		return rentRepository.findAll();
+	}
+
+	@Override
+	public Rent getRentById(Long id) {
+		Optional<Rent> rent = rentRepository.findById(id);
+		return rent.get();
+	}
+
+	@Override
+	public Rent delete(Long id) {
+		RentRepository rentRepository2 = rentRepository;
+		rentRepository2.deleteById(id);
+		System.out.println("Deleted Successfully");
+		return (Rent) rentRepository2;
+	}
+
+}
