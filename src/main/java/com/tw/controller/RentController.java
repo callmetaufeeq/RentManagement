bef9837cd4aa3a038b72aa568d6e4336d114d34f
Taufeeq <123618277+callmetaufeeq@users.noreply.github.com>
2023-07-16 11:41:13 +0530

    cross origin

--- src/main/java/com/tw/controller/RentController.java
+++ src/main/java/com/tw/controller/RentController.java
@@ -3,6 +3,7 @@ package com.tw.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import com.tw.service.RentService;
 import jakarta.websocket.server.PathParam;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/rent")
 public class RentController {
 
