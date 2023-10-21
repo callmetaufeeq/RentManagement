262fc8feba33372b814fbb2153129f5ee6e7c970
Taufeeq <123618277+callmetaufeeq@users.noreply.github.com>
2023-10-21 15:00:27 +0530

    query

--- src/main/java/com/tw/repository/RentSlaveRepository.java
+++ src/main/java/com/tw/repository/RentSlaveRepository.java
@@ -9,16 +9,15 @@ import com.tw.model.RentSlave;
 
 public interface RentSlaveRepository extends JpaRepository<RentSlave, Long>, JpaSpecificationExecutor<RentSlave> {
 
-
 	@Query("SELECT SUM(rs.paid) FROM RentSlave rs WHERE rs.shop.id = :shopId AND rs.year = :year AND rs.paymentType = :paymentType GROUP BY rs.shop.id")
-	Double getSumPaid(@Param("shopId") Long shopId, @Param("year") String year, @Param("paymentType") String paymentType);
+	Double getSumPaid(@Param("shopId") Long shopId, @Param("year") String year,
+			@Param("paymentType") String paymentType);
 
 	@Query("SELECT DISTINCT rs.rentAmount FROM RentSlave rs WHERE rs.shop.id = :shopId AND rs.year = :year AND rs.paymentType = :paymentType")
 	Double getRentAmount(@Param("shopId") Long shopId, @Param("year") String year,
 			@Param("paymentType") String paymentType);
 
-	@Query("SELECT rs.depositAmount FROM RentSlave rs WHERE rs.shop.id = :shopId AND rs.year = :year AND rs.paymentType = :paymentType")
+	@Query("SELECT DISTINCT rs.depositAmount FROM RentSlave rs WHERE rs.shop.id = :shopId AND rs.year = :year AND rs.paymentType = :paymentType")
 	Double getDepositAmount(@Param("shopId") Long shopId, @Param("year") String year,
 			@Param("paymentType") String paymentType);
-
 }
